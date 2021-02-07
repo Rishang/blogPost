c2e887de8f09917ad498684ef67295844fa2ea66
Rishang <rishangbhavsarcs@gmail.com>
2021-02-07 22:41:09 +0530

    using bash now

--- webapp/entrypoint.dev.sh
+++ webapp/entrypoint.dev.sh
@@ -1,23 +1,31 @@
-#!/bin/sh
+#!/bin/bash
+
+function _django_config {
+  
+ # migrate db and django
+  echo "migrate django , postgres"
+  python3 manage.py makemigrations
+  python3 manage.py migrate
+
+  # create superuser
+  echo "creating superuser"
+  python3 manage.py shell -c "exec(open('./django_blog/create_admin.py','r').read())"
+
+}
 
 if [ "$DATABASE" = "postgres" ]
 then
-    echo "Waiting for postgres..."
 
-    while ! nc -z $POSTGRES_HOSTNAME $DATABASE_PORT; do
-      sleep 0.1
-    done
+  echo "Waiting for postgres..."
 
-    echo "PostgreSQL started"
+  while ! nc -z $POSTGRES_HOSTNAME $DATABASE_PORT; do
+    sleep 0.1
+  done
 
-    # migrate db and django
-    echo "migrate django , postgres"
-    python3 manage.py makemigrations
-    python3 manage.py migrate
+  echo "PostgreSQL started"
 
-    # create superuser
-    echo "creating superuser"
-    python3 manage.py shell -c "exec(open('./django_blog/create_admin.py','r').read())"
+  _django_config
+   
 fi
 
 exec "$@"
